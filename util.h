c1db29a6b780877b31994c00ce665f1d113a999c
mirkoviviano <info@mirkoviviano.it>
2019-06-15 14:37:21 +0100

    Some bug fix

--- util.h
+++ util.h
@@ -7,7 +7,7 @@ class Util {
 public:
     static std::string convertToTime ( long int input_seconds );
     static std::string getProgressBar(std::string percent);
-    static std::ifstream getStream(std::string path, std::ifstream& stream);
+    static std::ifstream getStream(std::string path);
 };
 
 std::string Util::convertToTime (long int input_seconds){
@@ -46,11 +46,11 @@ std::string Util::getProgressBar(std::string percent){
 }
 
 // wrapper for creating streams
-std::ifstream Util::getStream(std::string path, std::ifstream& stream){
-    stream.open (path, std::ifstream::in);
-    if (!stream && !stream.is_open()){
-        stream.close();
+std::ifstream Util::getStream(std::string path){
+    std::ifstream stream(path);
+    if  (!stream) {ß Pr
         throw std::runtime_error("Non - existing PID");
     }
     return stream;
 }
+
